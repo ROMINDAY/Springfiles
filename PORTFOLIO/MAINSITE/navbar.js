ff6e939001616ce8127eaea862e2107127cb3440
Alex <90908712+ROMINDAY@users.noreply.github.com>
2023-11-29 15:09:55 +0100

    Add files via upload

---
+++ PORTFOLIO/MAINSITE/navbar.js
@@ -0,0 +1,20 @@
+// Hamburger menu
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
---
+++ PORTFOLIO/MAINSITE/photobank/script.js
@@ -0,0 +1,3 @@
+document.getElementById("btnClone").addEventListener("click", function(){
+    alert ("This is a clone site and not Instagram's official site. Please do not give your passwords to phishing sites!");
+})
